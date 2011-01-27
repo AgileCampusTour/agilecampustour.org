65056b74b3c414d104c0e7f6efda1346256fb85f
Marc Lainez <marc.lainez@gmail.com>
2011-01-27 20:51:54 +0100

    First commit

---
+++ act.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'sinatra/r18n'
+require 'haml'
+
+set :haml, :format => :html5
+set :default_locale, 'en'
+
+get '/' do
+  haml :index
+end
+
+get '/:locale' do
+  haml :index
+end
