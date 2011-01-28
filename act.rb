1d5a245d8f650f36083551e601aa8f278428aa88
Julien Biezemans <jb@jbpros.com>
2011-01-28 17:42:33 +0100

    Require credentials in staging environment

--- act.rb
+++ act.rb
@@ -9,6 +9,13 @@ require 'haml'
 set :haml, :format => :html5
 set :default_locale, 'en'
 
+if ENV['RACK_ENV'] == 'staging'
+  use Rack::Auth::Basic, "Agile Campus Tour staging server" do |username, password|
+    require 'digest/md5'
+    Digest::MD5.hexdigest("#{username}:#{password}") == "8977a908d090515c63b041f06e3a960e"
+  end
+end
+
 get '/' do
   haml :index
 end
