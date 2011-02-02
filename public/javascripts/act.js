d48905aca5fceeda8cd06d590afe1877d6584ac5
Julien Biezemans <jb@jbpros.com>
2011-02-03 00:17:47 +0100

    Styles

--- public/javascripts/act.js
+++ public/javascripts/act.js
@@ -2,7 +2,7 @@ var AgileCampusTour = {
   init: function() {
     $(".tweets").tweet({
       username: "agilecampustour",
-      count: 5,
+      count: 3,
       query: "agile",
       refresh_interval: 10,
       loading_text: "loading tweets..."
